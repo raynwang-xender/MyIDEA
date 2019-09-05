00ced930d13521c33e1d6e1fa247bd82d4a02b43
Rayn Wang <jjdawangrui@163.com>
2019-09-05 18:36:20 +0800

    first

---
+++ src/main/java/test/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package test.config;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+
+    /**    ---Rayn 固定写法，记就完事了   */
+    @Bean(name = "testDataSource")
+    @Qualifier("testDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.test")
+    public DataSource testDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+    @Bean(name = "testDataSource")
+    public JdbcTemplate testJdbcTemplate(@Qualifier("testDataSource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+}
--- src/main/java/test/dao/impl/TestDaoImpl.java
+++ src/main/java/test/dao/impl/TestDaoImpl.java
@@ -1,6 +1,8 @@
 package test.dao.impl;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import test.dao.TestDao;
 
@@ -9,11 +11,25 @@ import test.dao.TestDao;
 
 @Service
 public class TestDaoImpl implements TestDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String SQL =
+            "INSERT INTO tb_test (`uid`,`name`,`gender`) VALUES (6,'123',12)";
 
     @Override
     public void insert() {
         System.out.println(TAG+"---Rayn insert方法");
+
+        jdbcTemplate.execute(SQL);
     }
+
+
+
+
+
+
+
 
 
 }
