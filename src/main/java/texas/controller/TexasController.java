1dc5da7d62d5262184b852243b69fefb9d62e084
Rayn Wang <jjdawangrui@163.com>
2019-09-09 01:26:10 +0800

    Texas初步

--- src/main/java/texas/controller/TexasController.java
+++ src/main/java/texas/controller/TexasController.java
@@ -1,6 +1,7 @@
 package texas.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,17 @@ public class TexasController {
         userInfo.setWname(request.getParameter("wname"));
         texasService.save(userInfo);
     }
+
+
+    @RequestMapping(value = "/conf", method = {RequestMethod.POST, RequestMethod.GET})
+    public void conf(final HttpServletRequest request) {
+        System.out.println(TAG+"---Rayn conf");
+
+
+
+
+    }
+
+
 
 }
