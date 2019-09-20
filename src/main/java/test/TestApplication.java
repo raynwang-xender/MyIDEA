b18e67cd851b15596cbbcb46d5b1819aad51e026
Rayn Wang <jjdawangrui@163.com>
2019-09-20 18:35:31 +0800

    aaa

--- src/main/java/test/TestApplication.java
+++ src/main/java/test/TestApplication.java
@@ -1,12 +1,12 @@
-package test;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class,args);
-    }
-}
+//package test;
+//
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class TestApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(TestApplication.class,args);
+//    }
+//}
